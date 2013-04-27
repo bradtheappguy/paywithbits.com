2a71a28d0e2745d2f70d6da817fe2b7c7fe69339
Diclophis <diclophis@gmail.com>
2013-04-27 16:18:12 -0700

    add phone number model

---
+++ app/models/phone_number.rb
@@ -0,0 +1,10 @@
+class PhoneNumber < ActiveRecord::Base
+  attr_accessible :number, :wallet
+
+  validates_presence_of :number
+  validates_presence_of :wallet
+
+
+  validates :balance, :numericality => { :greater_than => 0.0 }
+
+end
---
+++ test/unit/phone_number_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class PhoneNumberTest < ActiveSupport::TestCase
+  test "a new phone number has 0.0 balance" do
+    new_number = PhoneNumber.new
+    assert new_number.balance == 0.0
+  end
+
+  test "a phone numbers balance must be greater than zero" do
+    new_number = PhoneNumber.new(:number => "something", :wallet => "else")
+    new_number.balance = -1
+    assert !new_number.valid?
+  end
+
+  test "a phone numbers balance must be updatable" do
+    new_number = PhoneNumber.new(:number => "something", :wallet => "else")
+    new_number.balance += 1.0
+    assert new_number.balance == 1.0
+    assert new_number.valid?
+    assert new_number.save
+    new_number.reload.balance == 1.0
+  end
+end
