da1c257232b94b970f582645c3f8b2016c7f9e96
Brad Smith <bradsmithinc@gmail.com>
2013-04-28 13:04:23 +0000

    helps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,7 @@ class ApplicationController < ActionController::Base
     unknown_request_message = "Unknown Request."
     syntax_message = "Invalid Syntax."
     same_person_message = "Same Person."
-    help_message = "See: http://paywithbits.com/help"
-
+    help_message = "\n\nCommands:\n balance \n send \n request \n invite"
     commands = {"signup" => "signup"}
     
     
